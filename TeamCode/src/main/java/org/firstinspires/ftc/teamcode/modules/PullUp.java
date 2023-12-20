d8ba060b5267f339b697af583b30fd426ff3567d
serg-tel <telpov.serg@yandex.ru>
2023-12-20 16:06:17 +0300

    PullUp

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/modules/PullUp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/modules/PullUp.java
@@ -1,7 +1,6 @@
 package org.firstinspires.ftc.teamcode.modules;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
-import com.qualcomm.robotcore.hardware.CRServo;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.Gamepad;
@@ -10,16 +9,13 @@ import com.qualcomm.robotcore.hardware.HardwareMap;
 import org.firstinspires.ftc.robotcore.external.Telemetry;
 
 public class PullUp {
-    private final LinearOpMode linearOpMode;
-    private final HardwareMap hardwareMap;
     private final Telemetry telemetry;
     private final Gamepad gamepad;
     private final DcMotor motor;
     private double tempPower = 0;
 
     public PullUp(LinearOpMode linearOpMode) {
-        this.linearOpMode = linearOpMode;
-        hardwareMap = linearOpMode.hardwareMap;
+        HardwareMap hardwareMap = linearOpMode.hardwareMap;
         telemetry = linearOpMode.telemetry;
         gamepad = linearOpMode.gamepad1;
 
