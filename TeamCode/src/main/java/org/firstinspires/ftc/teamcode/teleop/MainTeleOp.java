4cd57fde6c3d50023d78883c4fa507b25b6b5147
serg-tel <telpov.serg@yandex.ru>
2024-01-30 19:54:54 +0300

    Remove Gyro

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/MainTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/MainTeleOp.java
@@ -6,7 +6,6 @@ import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
 import org.firstinspires.ftc.teamcode.drive.OperatorDrive;
-import org.firstinspires.ftc.teamcode.drive.opmode.Gyroscope;
 import org.firstinspires.ftc.teamcode.modules.PullUp;
 import org.firstinspires.ftc.teamcode.modules.Scorer;
 import org.firstinspires.ftc.teamcode.modules.Intake;
@@ -19,7 +18,6 @@ public class MainTeleOp extends LinearOpMode {
     public Lift lift;
     public Intake intake;
     public Scorer scorer;
-    public Gyroscope gyroscope;
     public PullUp pullUp;
     public Shooter shooter;
     private final ElapsedTime runtime = new ElapsedTime();
@@ -42,7 +40,6 @@ public class MainTeleOp extends LinearOpMode {
             intake.opControlSensor();
             scorer.opControl();
             pullUp.opControlPos();
-            gyroscope.opControl();
             // shooter.tele();
 
             telemetry.addData("Runtime", runtime.toString());
